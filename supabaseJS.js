98d4f39b89e268e9a017d3408d70163da6350797
fieme-one <fieme1145@gmail.com>
2025-11-17 17:17:17 +0300

    Add files via upload

---
+++ supabaseJS.js
@@ -0,0 +1,7 @@
+// supabaseClient.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
+
+const supabaseUrl = "https://jtimdeudmkeffendjczz.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0aW1kZXVkbWtlZmZlbmRqY3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMTYxNzQsImV4cCI6MjA3ODg5MjE3NH0.6cqsAzO8DvnjIvg4Urb6Ld1W2xe3FBI8s2yvFMdwox8"
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
